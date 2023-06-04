06e007e4867a543c999cde0282ca80b8a6d3ff03
Roman160784 <matveyenkaraman@gmail.com>
2023-06-04 13:03:20 +0300

    deploy

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -16,7 +16,7 @@ bot.on('message', async (ctx) => {
     try {
         const bill = ctx.message.text
         if (bill.length !== 10) {
-            ctx.reply(`Не верные данные 😔`)
+            ctx.reply(`Я не на столько умён чтобы выполнить эту команду 😔`)
         } else {
             const url = `https://www.gomelcsms.by/info/scheta.php?nom=${bill}&god=2023`
             const res = await axios.get(url)
